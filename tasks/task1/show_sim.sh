7fca5b88e98a23c17b4c3b126bcc8dff01436627
DongkyunLim <fisma@FISMAs-MBA.local>
2024-08-13 16:07:14 +0900

    1-task1

--- tasks/task1/show_sim.sh
+++
@@ -1,4 +0,0 @@
-for i in $(seq 0.025 0.025 1.000)
-do
-    grep "Packet latency average" ./latency_uni_ff_${i} | grep sample | awk '{print $5}'
-done
--- tasks/task1/task1.sh
+++
@@ -1,4 +0,0 @@
-for i in $(seq 0.025 0.025 1.000)
-do
-    ../../booksim2/src/booksim ../../booksim2/src/examples/flatflyconfig sim_type=latency traffic=uniform injection_rate={$i} > ./latency_uni_ff_${i}
-done
